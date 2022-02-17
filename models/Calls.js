fea1876acaee1e2c12129498ed4e45dd00f84ee5
unknown <batista romao>
2022-02-16 23:57:30 -0300

    criando validações de erros/respostas para alguns campos de entradas

--- models/Calls.js
+++ models/Calls.js
@@ -3,20 +3,43 @@ const conn = require('../below/connect')
 
 class Calls {
     createCalls(call, res) {
-        console.log(call)
-
         const dateCreate = moment().format('YYYY-MM-DD HH:MM:SS')
         const date = moment(call.date, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS')
-        const callData = {...call, date, dateCreate}
-        const sql = 'INSERT INTO calls SET ? '
+        const datevalid = moment(date).isSameOrAfter(dateCreate)
+        const clientValid = call.client.length >= 5
 
-        conn.query(sql, callData, (error, result) => {
-            if (error) {
-                res.status(400).json(error);
-            } else {
-                res.status(201).json(result);
-            }
-        })
+        const validate = [
+            {
+                nome: "data",
+                valid: datevalid,
+                mesage: "A data de agendamento deve ser maior ou igual a hora do agendamento"
+            },
+            {
+                nome: "client",
+                valid: clientValid,
+                mesage: "O nome do cliente deve ser um nome real (acima de 3 caracteres)"
+            },
+
+        ]
+
+        const erros = validate.filter(campo => !campo.valid)
+        const thereAreErrors = erros.length
+
+        if (thereAreErrors) {
+            res.status(400).json(erros);
+        } else {
+            const callData = {...call, date, dateCreate}
+            const sql = 'INSERT INTO calls SET ? '
+    
+            conn.query(sql, callData, (error, result) => {
+                if (error) {
+                    res.status(400).json(error);
+                } else {
+                    res.status(201).json(result);
+                }
+            })
+        }
+
     }
 }
 
